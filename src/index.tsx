a5ce86fb2e05ccb6bcdac1fe1ad644bd17a2835f
Catzilla <jewaysongonzalgo@gmail.com>
2022-03-21 14:50:05 +0800

    Add list of Todo that for further improvement

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
+// TODO: use material-ui as the UI framework
+// TODO: further enhance prettier
+// TODO: further enhance eslint if we can add more improvements
+// TODO: re-check webpack configuration
+// TODO: provide an initial design for the UI
+// TODO: decide whether to use socket.io or REST
+
 ReactDOM.render(<App />, document.getElementById("root"));
