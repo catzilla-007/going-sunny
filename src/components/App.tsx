e52b0bd102ea4691b1b5a11b8425a0419c70df02
Catzilla <jewaysongonzalgo@gmail.com>
2022-03-22 12:48:11 +0800

    Add initial material ui theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from "react";
 
+import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+
+import customTheme from "../theme";
 
 const App: FC = () => (
-  <CssBaseline>
-    <h1>Going Sunny</h1>
-  </CssBaseline>
+  <ThemeProvider theme={customTheme}>
+    <CssBaseline>
+      <Typography variant="h2">Going Sunny</Typography>
+    </CssBaseline>
+  </ThemeProvider>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-// TODO: use material-ui as the UI framework
 // TODO: further enhance prettier
 // TODO: further enhance eslint if we can add more improvements
 // TODO: re-check webpack configuration
